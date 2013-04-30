3c8cbdc21a15bd03a70b1538e86706c2484a26db
Pawel Jedruch <Pawel.Jedruch@sabre.com>
2013-04-30 16:43:17 +0200

    draft of first column ruler for abstract text editor

---
+++ com.jedruch.eclipse.gitgutter/src/com/jedruch/eclipse/gitgutter/GitGutterRulerColumn.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2013, Sabre Inc.
+ */
+package com.jedruch.eclipse.gitgutter;
+
+import org.eclipse.jface.text.source.LineNumberRulerColumn;
+
+/**
+ * @author Pawel Jedruch
+ * 
+ */
+public class GitGutterRulerColumn extends LineNumberRulerColumn {
+
+    @Override
+    protected String createDisplayString(int line) {
+        return "x" + super.createDisplayString(line);
+    }
+
+    @Override
+    protected int computeNumberOfDigits() {
+        return super.computeNumberOfDigits() + 1;
+    }
+
+}
